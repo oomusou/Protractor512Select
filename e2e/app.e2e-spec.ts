67178e2129890ef95174488dd40670447ed657b9
Oomusou <oomusou@gmail.com>
2017-08-26 15:46:40 +0800

    Acceptance Test (failed)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -5,10 +5,29 @@ describe('protractor512-select App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it(`should have '1' select`, () => {
+    expect(page.getSelect().isPresent()).toBe(true);
+  });
+
+  it(`should have '3' options in select`, () => {
+    expect(page.getSelectCount()).toBe(3);
+  });
+
+  it(`should show '0' when selecting 'AWS'`, () => {
+    page.selectCloudByIndex(0);
+    expect(page.getSelectedId()).toBe('0');
+  });
+
+  it(`should show '1' when selecting 'Azure'`, () => {
+    page.selectCloudByText('Azure');
+    expect(page.getSelectedId()).toBe('1');
+  });
+
+  it(`should show '2' when selecting 'GCP'`, () => {
+    page.selectCloudByText('GCP');
+    expect(page.getSelectedId()).toBe('2');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,38 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getSelect(): ElementFinder {
+    return element(by.id('TDDSelect'));
+  }
+
+  getSelectCount(): any {
+    return element(by.id('TDDSelect'))
+      .all(by.tagName('option'))
+      .count();
+  }
+
+  selectCloudByIndex(index: number): AppPage {
+    element(by.id('TDDSelect'))
+      .all(by.tagName('option'))
+      .get(index)
+      .click();
+
+    return this;
+  }
+
+  selectCloudByText(text: string): AppPage {
+    element(by.id('TDDSelect'))
+      .element(by.cssContainingText('option', text))
+      .click();
+
+    return this;
+  }
+
+  getSelectedId(): any {
+    return element(by.css('p')).getText();
   }
 }
